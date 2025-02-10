1ee884257e504a40c7111c86654b717f11a81124
Vlad Alistar <vladalistar@gmail.com>
2025-02-10 21:59:44 +0200

    Update pyproject.toml, setup.py, and MANIFEST.in for PyPI release

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@ from setuptools import find_packages, setup
 setup(
     name="pythonfacebook",
     version="0.1.0",
-    description="A Python library for interacting with Facebook API.",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    author="Vlad Andrei",
-    author_email="contact@vlad.my",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    install_requires=open("requirements.txt").read().splitlines(),
+    install_requires=[
+        "requests",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.10",
+    exclude_package_data={
+        "": ["tests/*"],
+    },
 )
