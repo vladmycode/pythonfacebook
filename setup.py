cba0a6e6457612e52b7016936d14121fd31a0d04
Vlad Alistar <vladalistar@gmail.com>
2025-02-10 22:07:32 +0200

    Updated package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="pythonfacebook",
+    name="python-facebook-lite",
     version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
